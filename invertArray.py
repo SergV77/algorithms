ac10ca4c284b5b8dd36fb4fec56c49259893e7bd
Сергей Полещук <sergv77@yandex.ru>
2021-08-13 15:47:14 +0300

    Создана функция invert_array

--- invertArray.py
+++ invertArray.py
@@ -1,16 +1,16 @@
 
-def invert_array(array):
+def invert_array(array, N):
     """ Алгоритм перестановки элементов массива.
     :param array: входящий массив
     :return: возврат массива в обратном порядке
     """
-    for i in range(len(array)//2):
-        array[i], array[len(array)-1-i] = array[len(array)-1-i], array[i]
+    for i in range(N//2):
+        array[i], array[N-1-i] = array[N-1-i], array[i]
 
     return array
 
 #test
 a = [1, 2, 3, 4, 5]
 b = [1, 2, 3, 4, 5, 6]
-print(invert_array(a))
-print(invert_array(b))
+print(invert_array(a, len(a)))
+print(invert_array(b, len(b)))
