c26bc250d052b3a0c5041f1a6adfb9cfa7c5a471
SergV77 <SergV77@yandex.ru>
2021-09-04 20:11:26 +0300

    Добавлен файл теста

---
+++ test.py
@@ -0,0 +1,15 @@
+pow = lambda x: x*x
+data = 1
+
+for i in range(1, 3):
+    data += pow(i)
+
+print(pow(data))
+
+# dict_c = {'1': 3, '2': 2, '3': 1}
+# for key in dict_c.keys():
+#     if key == '2':
+#         del dict_c[key]
+#
+# print(dict_c)
+#
